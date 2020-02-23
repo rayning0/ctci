c02d7c7971b49b2885dc5b9f1eb3908fc7088559
Raymond Gan <rayning@yahoo.com>
2020-02-23 03:32:16 -0800

    Final, best answer for Critical Connections in a Network

--- leetcode/critical_connections_in_a_network_spec.rb
+++ leetcode/critical_connections_in_a_network_spec.rb
@@ -1,40 +1,31 @@
 # https://leetcode.com/problems/critical-connections-in-a-network/
+# https://www.coursera.org/lecture/algorithms-part2/connected-components-Dzl65
 # https://www.youtube.com/watch?v=aZXi1unBdJA (Bridges algorithm)
 # https://www.youtube.com/watch?v=V6kRqdtM_Uk (Bridges source code)
-# This test fails: https://leetcode.com/submissions/detail/305923798/testcase/
+
 # O(V + E): vertices + edges
+# Runtime: 784 ms, faster than 70.00% of Ruby online submissions for Critical Connections in a Network.
+# Memory Usage: 94.9 MB, less than 100.00% of Ruby online submissions for Critical Connections in a Network.
 class Graph
-  attr_accessor :n, :id, :connections, :alist, :visited, :lowlink, :bridges
+  attr_accessor :n, :id, :ids, :connections, :graph, :visited, :lowlink, :bridges
 
   def initialize(n, connections)
-    @connections = connections
-    @n = n # number of nodes
-    puts "Starting Connections: #{connections}"
-
-    # Adjacency List view of a graph
-    # https://www.geeksforgeeks.org/graph-and-its-representations/
-    @alist = adjacency_list
-    puts "Starting Adjacency List: #{alist}"
-
+    @id, @n, @connections, @bridges = -1, n, connections, []
+    @ids, @visited = Array.new(n, 0), Array.new(n, false)
     @lowlink = Array.new(n, 0) # smallest node reachable from current node, including itself
-    @visited = Array.new(n, false)
-    @id = 0
-  end
+    @graph = Hash.new{ |hash, key| hash[key] = [] }
 
-  # find all bridges in graph across connected components
-  def find_bridges
-    @bridges = []
-    n.times do |node|
-      puts "node: #{node}, bridges: #{bridges}"
-      dfs(node, -1) if !visited[node]
+    # undirected graph automatically points both directions
+    connections.each do |u, v|
+      @graph[u] << v
+      @graph[v] << u
     end
 
-    puts "\nlowlink: #{lowlink}"
-    puts "-----------------------\n\n"
-    bridges
+    # puts "Connections: #{connections}"
+    # puts "Graph: #{graph}"
   end
 
-  # ---depth-first search to find bridges---
+  # ---depth-first search to find bridges across connected components---
   # current: node now
   # previous: node before
   # to: next node
@@ -42,46 +33,42 @@ class Graph
     visited[current] = true
     @id += 1
     lowlink[current] = id
-    puts "current: #{current}, adjacent: #{alist[current]}, lowlink: #{lowlink}, visited: #{visited}, id: #{id}"
+    ids[current] = id
+
+    # puts "\ncurrent: #{current}, ids: #{ids}, lowlink: #{lowlink}"
+    # puts "adjacent: #{graph[current]}, visited: #{visited}, id: #{id}"
 
     # for each adjacent edge from 'current' to 'to'
-    alist[current].each do |to|
-      puts "to: #{to}"
+    graph[current].each do |to|
+      # puts "to: #{to}"
       next if to == previous # don't return to previous node (for undirected graph)
       if !visited[to]
         dfs(to, current)
 
-        # track smallest node we may reach from current
+        # Propagate smallest node we may reach from current
         lowlink[current] = [lowlink[current], lowlink[to]].min
-        puts "lowlink[#{current}]: #{lowlink[current]}"
+        # puts "lowlink: #{lowlink}. lowlink[#{current}]: #{lowlink[current]}"
 
-        # If no way to drop from 'to' node to current node #, it means
+        # If no way to drop from 'to' node to current node #,
         # there's no path back to current node. Thus we found a BRIDGE!
-        if current < lowlink[to]
+        if ids[current] < lowlink[to]
           bridges << [current, to]
-          puts "bridges: #{bridges}. current = #{current} < lowlink[to = #{to}] = #{lowlink[to]}"
+          # puts "------>> bridges: #{bridges} <<------- ids[#{current}] (#{ids[current]}) < lowlink[#{to}] (#{lowlink[to]})"
         end
       else
-        puts "** visited all adjacent nodes. drop lowlink[current]: #{lowlink[current]} if 'to' = #{to} < lowlink[#{current}] = #{lowlink[current]} **"
-        lowlink[current] = [lowlink[current], to].min
-        puts "lowlink[#{current}]: #{lowlink[current]}"
+        # puts "** visited all adjacent nodes. drop lowlink[current] if lowlink[#{to}] (#{lowlink[to]}) < lowlink[#{current}] (#{lowlink[current]}) **"
+        lowlink[current] = [lowlink[current], lowlink[to]].min
+        # puts "lowlink: #{lowlink}. lowlink[#{current}]: #{lowlink[current]}"
       end
     end
-  end
 
-  def adjacency_list
-    alist = {}
-    connections.each do |a, b|
-      alist[a] ? alist[a] << b : alist[a] = [b]
-      alist[b] ? alist[b] << a : alist[b] = [a]
-    end
-
-    alist
+    bridges
   end
 end
 
 def critical_connections(n, connections)
-  Graph.new(n, connections).find_bridges
+  # puts "----------------------------------------------\n\n"
+  Graph.new(n, connections).dfs(0, -1)
 end
 
 describe "it gives all critical connections in the network" do
@@ -98,124 +85,160 @@ describe "it gives all critical connections in the network" do
   end
 end
 
-# Starting Connections: [[0, 1], [1, 2], [2, 0], [1, 3]]
-# Starting Adjacency List: {0=>[1, 2], 1=>[0, 2, 3], 2=>[1, 0], 3=>[1]}
-# node: 0, bridges: []
-# current: 0, adjacent: [1, 2], lowlink: [1, 0, 0, 0], visited: [true, false, false, false], id: 1
-# to: 1
-# current: 1, adjacent: [0, 2, 3], lowlink: [1, 2, 0, 0], visited: [true, true, false, false], id: 2
-# to: 0
-# to: 2
-# current: 2, adjacent: [1, 0], lowlink: [1, 2, 3, 0], visited: [true, true, true, false], id: 3
-# to: 1
-# to: 0
-# ** visited all adjacent nodes. drop lowlink[current]: 3 if 'to' = 0 < lowlink[2] = 3 **
-# lowlink[2]: 0
-# lowlink[1]: 0
-# to: 3
-# current: 3, adjacent: [1], lowlink: [1, 0, 0, 4], visited: [true, true, true, true], id: 4
-# to: 1
-# lowlink[1]: 0
-# bridges: [[1, 3]]. current = 1 < lowlink[to = 3] = 4
-# lowlink[0]: 0
-# to: 2
-# ** visited all adjacent nodes. drop lowlink[current]: 0 if 'to' = 2 < lowlink[0] = 0 **
-# lowlink[0]: 0
-# node: 1, bridges: [[1, 3]]
-# node: 2, bridges: [[1, 3]]
-# node: 3, bridges: [[1, 3]]
+# Same code with no comments:
 
-# lowlink: [0, 0, 0, 4]
-# -----------------------
+# def dfs(current, previous)
+#   visited[current] = true
+#   @id += 1
+#   lowlink[current] = id
+#   ids[current] = id
 
-# Starting Connections: [[0, 1], [1, 2], [2, 0], [1, 3], [3, 4], [4, 5], [5, 3]]
-# Starting Adjacency List: {0=>[1, 2], 1=>[0, 2, 3], 2=>[1, 0], 3=>[1, 4, 5], 4=>[3, 5], 5=>[4, 3]}
-# node: 0, bridges: []
-# current: 0, adjacent: [1, 2], lowlink: [1, 0, 0, 0, 0, 0], visited: [true, false, false, false, false, false], id: 1
+#   graph[current].each do |to|
+#     next if to == previous
+#     if !visited[to]
+#       dfs(to, current)
+#       lowlink[current] = [lowlink[current], lowlink[to]].min
+
+#       if ids[current] < lowlink[to]
+#         bridges << [current, to]
+#       end
+#     else
+#       lowlink[current] = [lowlink[current], lowlink[to]].min
+#     end
+#   end
+
+#   bridges
+# end
+
+# Connections: [[0, 1], [1, 2], [2, 0], [1, 3]]
+# Graph: {0=>[1, 2], 1=>[0, 2, 3], 2=>[1, 0], 3=>[1]}
+
+# current: 0, ids: [0, 0, 0, 0], lowlink: [0, 0, 0, 0]
+# adjacent: [1, 2], visited: [true, false, false, false], id: 0
 # to: 1
-# current: 1, adjacent: [0, 2, 3], lowlink: [1, 2, 0, 0, 0, 0], visited: [true, true, false, false, false, false], id: 2
+
+# current: 1, ids: [0, 1, 0, 0], lowlink: [0, 1, 0, 0]
+# adjacent: [0, 2, 3], visited: [true, true, false, false], id: 1
 # to: 0
 # to: 2
-# current: 2, adjacent: [1, 0], lowlink: [1, 2, 3, 0, 0, 0], visited: [true, true, true, false, false, false], id: 3
+
+# current: 2, ids: [0, 1, 2, 0], lowlink: [0, 1, 2, 0]
+# adjacent: [1, 0], visited: [true, true, true, false], id: 2
 # to: 1
 # to: 0
-# ** visited all adjacent nodes. drop lowlink[current]: 3 if 'to' = 0 < lowlink[2] = 3 **
-# lowlink[2]: 0
-# lowlink[1]: 0
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[0] (0) < lowlink[2] (2) **
+# lowlink: [0, 1, 0, 0]. lowlink[2]: 0
+# lowlink: [0, 0, 0, 0]. lowlink[1]: 0
 # to: 3
-# current: 3, adjacent: [1, 4, 5], lowlink: [1, 0, 0, 4, 0, 0], visited: [true, true, true, true, false, false], id: 4
+
+# current: 3, ids: [0, 1, 2, 3], lowlink: [0, 0, 0, 3]
+# adjacent: [1], visited: [true, true, true, true], id: 3
+# to: 1
+# lowlink: [0, 0, 0, 3]. lowlink[1]: 0
+# ------>> bridges: [[1, 3]] <<------- ids[1] (1) < lowlink[3] (3)
+# lowlink: [0, 0, 0, 3]. lowlink[0]: 0
+# to: 2
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[2] (0) < lowlink[0] (0) **
+# lowlink: [0, 0, 0, 3]. lowlink[0]: 0
+
+# ----------------------------------------------
+
+# Connections: [[0, 1], [1, 2], [2, 0], [1, 3], [3, 4], [4, 5], [5, 3]]
+# Graph: {0=>[1, 2], 1=>[0, 2, 3], 2=>[1, 0], 3=>[1, 4, 5], 4=>[3, 5], 5=>[4, 3]}
+
+# current: 0, ids: [0, 0, 0, 0, 0, 0], lowlink: [0, 0, 0, 0, 0, 0]
+# adjacent: [1, 2], visited: [true, false, false, false, false, false], id: 0
+# to: 1
+
+# current: 1, ids: [0, 1, 0, 0, 0, 0], lowlink: [0, 1, 0, 0, 0, 0]
+# adjacent: [0, 2, 3], visited: [true, true, false, false, false, false], id: 1
+# to: 0
+# to: 2
+
+# current: 2, ids: [0, 1, 2, 0, 0, 0], lowlink: [0, 1, 2, 0, 0, 0]
+# adjacent: [1, 0], visited: [true, true, true, false, false, false], id: 2
+# to: 1
+# to: 0
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[0] (0) < lowlink[2] (2) **
+# lowlink: [0, 1, 0, 0, 0, 0]. lowlink[2]: 0
+# lowlink: [0, 0, 0, 0, 0, 0]. lowlink[1]: 0
+# to: 3
+
+# current: 3, ids: [0, 1, 2, 3, 0, 0], lowlink: [0, 0, 0, 3, 0, 0]
+# adjacent: [1, 4, 5], visited: [true, true, true, true, false, false], id: 3
 # to: 1
 # to: 4
-# current: 4, adjacent: [3, 5], lowlink: [1, 0, 0, 4, 5, 0], visited: [true, true, true, true, true, false], id: 5
+
+# current: 4, ids: [0, 1, 2, 3, 4, 0], lowlink: [0, 0, 0, 3, 4, 0]
+# adjacent: [3, 5], visited: [true, true, true, true, true, false], id: 4
 # to: 3
 # to: 5
-# current: 5, adjacent: [4, 3], lowlink: [1, 0, 0, 4, 5, 6], visited: [true, true, true, true, true, true], id: 6
+
+# current: 5, ids: [0, 1, 2, 3, 4, 5], lowlink: [0, 0, 0, 3, 4, 5]
+# adjacent: [4, 3], visited: [true, true, true, true, true, true], id: 5
 # to: 4
 # to: 3
-# ** visited all adjacent nodes. drop lowlink[current]: 6 if 'to' = 3 < lowlink[5] = 6 **
-# lowlink[5]: 3
-# lowlink[4]: 3
-# lowlink[3]: 3
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[3] (3) < lowlink[5] (5) **
+# lowlink: [0, 0, 0, 3, 4, 3]. lowlink[5]: 3
+# lowlink: [0, 0, 0, 3, 3, 3]. lowlink[4]: 3
+# lowlink: [0, 0, 0, 3, 3, 3]. lowlink[3]: 3
 # to: 5
-# ** visited all adjacent nodes. drop lowlink[current]: 3 if 'to' = 5 < lowlink[3] = 3 **
-# lowlink[3]: 3
-# lowlink[1]: 0
-# bridges: [[1, 3]]. current = 1 < lowlink[to = 3] = 3
-# lowlink[0]: 0
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[5] (3) < lowlink[3] (3) **
+# lowlink: [0, 0, 0, 3, 3, 3]. lowlink[3]: 3
+# lowlink: [0, 0, 0, 3, 3, 3]. lowlink[1]: 0
+# ------>> bridges: [[1, 3]] <<------- ids[1] (1) < lowlink[3] (3)
+# lowlink: [0, 0, 0, 3, 3, 3]. lowlink[0]: 0
 # to: 2
-# ** visited all adjacent nodes. drop lowlink[current]: 0 if 'to' = 2 < lowlink[0] = 0 **
-# lowlink[0]: 0
-# node: 1, bridges: [[1, 3]]
-# node: 2, bridges: [[1, 3]]
-# node: 3, bridges: [[1, 3]]
-# node: 4, bridges: [[1, 3]]
-# node: 5, bridges: [[1, 3]]
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[2] (0) < lowlink[0] (0) **
+# lowlink: [0, 0, 0, 3, 3, 3]. lowlink[0]: 0
 
-# lowlink: [0, 0, 0, 3, 3, 3]
-# -----------------------
+# ----------------------------------------------
 
-# Starting Connections: [[1, 0], [2, 0], [3, 2], [4, 2], [4, 3], [3, 0], [4, 0]]
-# Starting Adjacency List: {1=>[0], 0=>[1, 2, 3, 4], 2=>[0, 3, 4], 3=>[2, 4, 0], 4=>[2, 3, 0]}
-# node: 0, bridges: []
-# current: 0, adjacent: [1, 2, 3, 4], lowlink: [1, 0, 0, 0, 0], visited: [true, false, false, false, false], id: 1
+# Connections: [[1, 0], [2, 0], [3, 2], [4, 2], [4, 3], [3, 0], [4, 0]]
+# Graph: {1=>[0], 0=>[1, 2, 3, 4], 2=>[0, 3, 4], 3=>[2, 4, 0], 4=>[2, 3, 0]}
+
+# current: 0, ids: [0, 0, 0, 0, 0], lowlink: [0, 0, 0, 0, 0]
+# adjacent: [1, 2, 3, 4], visited: [true, false, false, false, false], id: 0
 # to: 1
-# current: 1, adjacent: [0], lowlink: [1, 2, 0, 0, 0], visited: [true, true, false, false, false], id: 2
-# to: 0
-# lowlink[0]: 1
-# bridges: [[0, 1]]. current = 0 < lowlink[to = 1] = 2
-# to: 2
-# current: 2, adjacent: [0, 3, 4], lowlink: [1, 2, 3, 0, 0], visited: [true, true, true, false, false], id: 3
-# to: 0
-# to: 3
-# current: 3, adjacent: [2, 4, 0], lowlink: [1, 2, 3, 4, 0], visited: [true, true, true, true, false], id: 4
-# to: 2
-# to: 4
-# current: 4, adjacent: [2, 3, 0], lowlink: [1, 2, 3, 4, 5], visited: [true, true, true, true, true], id: 5
-# to: 2
-# ** visited all adjacent nodes. drop lowlink[current]: 5 if 'to' = 2 < lowlink[4] = 5 **
-# lowlink[4]: 2
-# to: 3
-# to: 0
-# ** visited all adjacent nodes. drop lowlink[current]: 2 if 'to' = 0 < lowlink[4] = 2 **
-# lowlink[4]: 0
-# lowlink[3]: 0
-# to: 0
-# ** visited all adjacent nodes. drop lowlink[current]: 0 if 'to' = 0 < lowlink[3] = 0 **
-# lowlink[3]: 0
-# lowlink[2]: 0
-# to: 4
-# ** visited all adjacent nodes. drop lowlink[current]: 0 if 'to' = 4 < lowlink[2] = 0 **
-# lowlink[2]: 0
-# lowlink[0]: 0
-# to: 3
-# ** visited all adjacent nodes. drop lowlink[current]: 0 if 'to' = 3 < lowlink[0] = 0 **
-# lowlink[0]: 0
-# to: 4
-# ** visited all adjacent nodes. drop lowlink[current]: 0 if 'to' = 4 < lowlink[0] = 0 **
-# lowlink[0]: 0
-# node: 1, bridges: [[0, 1]]
-# node: 2, bridges: [[0, 1]]
-# node: 3, bridges: [[0, 1]]
-# node: 4, bridges: [[0, 1]]
 
-# lowlink: [0, 2, 0, 0, 0]
+# current: 1, ids: [0, 1, 0, 0, 0], lowlink: [0, 1, 0, 0, 0]
+# adjacent: [0], visited: [true, true, false, false, false], id: 1
+# to: 0
+# lowlink: [0, 1, 0, 0, 0]. lowlink[0]: 0
+# ------>> bridges: [[0, 1]] <<------- ids[0] (0) < lowlink[1] (1)
+# to: 2
+
+# current: 2, ids: [0, 1, 2, 0, 0], lowlink: [0, 1, 2, 0, 0]
+# adjacent: [0, 3, 4], visited: [true, true, true, false, false], id: 2
+# to: 0
+# to: 3
+
+# current: 3, ids: [0, 1, 2, 3, 0], lowlink: [0, 1, 2, 3, 0]
+# adjacent: [2, 4, 0], visited: [true, true, true, true, false], id: 3
+# to: 2
+# to: 4
+
+# current: 4, ids: [0, 1, 2, 3, 4], lowlink: [0, 1, 2, 3, 4]
+# adjacent: [2, 3, 0], visited: [true, true, true, true, true], id: 4
+# to: 2
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[2] (2) < lowlink[4] (4) **
+# lowlink: [0, 1, 2, 3, 2]. lowlink[4]: 2
+# to: 3
+# to: 0
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[0] (0) < lowlink[4] (2) **
+# lowlink: [0, 1, 2, 3, 0]. lowlink[4]: 0
+# lowlink: [0, 1, 2, 0, 0]. lowlink[3]: 0
+# to: 0
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[0] (0) < lowlink[3] (0) **
+# lowlink: [0, 1, 2, 0, 0]. lowlink[3]: 0
+# lowlink: [0, 1, 0, 0, 0]. lowlink[2]: 0
+# to: 4
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[4] (0) < lowlink[2] (0) **
+# lowlink: [0, 1, 0, 0, 0]. lowlink[2]: 0
+# lowlink: [0, 1, 0, 0, 0]. lowlink[0]: 0
+# to: 3
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[3] (0) < lowlink[0] (0) **
+# lowlink: [0, 1, 0, 0, 0]. lowlink[0]: 0
+# to: 4
+# ** visited all adjacent nodes. drop lowlink[current] if lowlink[4] (0) < lowlink[0] (0) **
+# lowlink: [0, 1, 0, 0, 0]. lowlink[0]: 0
