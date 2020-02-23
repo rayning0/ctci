7b7d7e8646ef198e85cd0dc033bbeaebb9acfb90
Raymond Gan <rayning@yahoo.com>
2020-02-23 12:48:56 -0800

    Better JS solution

--- leetcode/longest_palindrome_spec.rb
+++ leetcode/longest_palindrome_spec.rb
@@ -44,3 +44,21 @@ describe '#longest_palindrome' do
     expect(longest("asdkasdfkxzclxlweieieisweiews")).to eq 'sweiews'
   end
 end
+
+# var longestPalindrome = function(s) {
+#   var max = '';
+#   for (var i = 0; i < s.length; i++) {
+#     for (var j = 0; j < 2; j++) {
+#       var left = i;
+#       var right = i + j;
+#       while (s[left] && s[left] === s[right]) {
+#         left--;
+#         right++;
+#       }
+#       if ((right - left - 1) > max.length) {
+#         max = s.substring(left + 1, right);
+#       }
+#     }
+#   }
+#   return max;
+# };
