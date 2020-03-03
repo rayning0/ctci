f60895c883346b8bf91a88bcb2b1b14fc7b255ec
Raymond Gan <rayning@yahoo.com>
2020-03-03 08:46:51 -0800

    Working 'Greatest Common Divisor of Strings'

--- leetcode/greatest_common_divisor_strings.test.js
+++ leetcode/greatest_common_divisor_strings.test.js
@@ -1,56 +1,37 @@
 // https://leetcode.com/problems/greatest-common-divisor-of-strings/
 // str1, str2 must be multiple of output, but they don't need to be multiples of each other
-// strategy:
-// 1. Make 1 cursors (or pointers), one for each string
-// 2. Move both cursors from left to right, comparing characters. if characters differ before we reach end of shorter string, output = ""
-// 3. Keep track of string. If see any substring that repeats, treat that as possible output.
-// 4. Otherwise, if we hit end of shorter string, use it as possible output against longer string.
-// 5. If shorter string is possible output, check if it repeats in longer string. Check if length of longer string is multiple of shorter string length.
+// Runtime: 52 ms, faster than 90.73 % of JavaScript online submissions for Greatest Common Divisor of Strings.
+// Memory Usage: 35.4 MB, less than 100.00 % of JavaScript online submissions for Greatest Common Divisor of Strings.
 
-// O(n)
+// Euclidean algorithm for Greatest Common Divisor:
+// https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
+// If A = 0 then GCD(A, B) = B, since the GCD(0, B) = B.
+// If B = 0 then GCD(A, B) = A, since the GCD(A, 0) = A.
+// GCD(A, B) = GCD(B, A % B)
+
+// Ex: gcd(100, 60) = gcd(60, 100 % 60 = 40) = gcd(40, 60 % 40 = 20) = gcd(20, 40 % 20 = 0) = 20
+
+function gcd(a, b) {
+  if (a == 0) return b
+  if (b == 0) return a
+  if (b > a) [a, b] = [b, a]
+
+  return gcd(b, a % b)
+}
+
+// Time: O(n). Space: O(1).
 function gcd_strings(str1, str2) {
-  let [start, end, oldstart] = [0, 0, 0]
   let [longer, shorter] = (str1.length > str2.length) ? [str1, str2] : [str2, str1]
-  let output = ""
-  let hash = {}
+  let gcd2 = gcd(str1.length, str2.length)
+  // console.log(`GCD: ${gcd2}`)
 
-  while (end < shorter.length) {
-    if (str1[end] != str2[end]) {
-      return ""
-    }
-    let char = shorter[end]
-    char in hash ? hash[char]++ : hash[char] = 1
-
-    if (shorter[start] == char && hash[char] != output.length + 1 && end > 0) {
-      oldstart = start
-      while (shorter[start] == shorter[end]) {
-        start++
-        end++
-      }
-      console.log("* ", shorter.substring(start, end), output, start,end)
-      if (shorter.substring(start, end) != output) {
-        start = oldstart
-      }
-      end--
-    }
-    end++
-    output = shorter.substring(start, end)
-    console.log(output, start, end)
+  for (let i = 0; i < longer.length; i++) {
+    j = i % gcd2 // lets index of shorter string start over from 0 when i >= shorter.length
+    // console.log(`i: ${i}, longer[i]: ${longer[i]}, j: ${j}, shorter[j]: ${shorter[j]}`)
+    if (longer[i] != shorter[j]) { return "" }
   }
 
-  start = 0
-  while (end < longer.length) {
-    console.log(output, start, longer, end)
-    if (output[start] != longer[end]) {
-      return ""
-    }
-    start++
-    end++
-    if (start == output.length) {
-      start = 0
-    }
-  }
-  return output
+  return shorter.substring(0, gcd2)
 }
 
 describe ("#gcd_strings", () => {
@@ -72,8 +53,38 @@ describe ("#gcd_strings", () => {
     expect(gcd_strings("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX")).toEqual("TAUXX")
     expect(gcd_strings("FFBNXKSTFFBNXKSTFFBNXKSTFFBNXKSTFFBNXKST",
       "FFBNXKSTFFBNXKSTFFBNXKSTFFBNXKSTFFBNXKSTFFBNXKSTFFBNXKSTFFBNXKSTFFBNXKST")).toEqual("FFBNXKST")
-
-    // "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-    // "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
   })
 })
+
+// expect(gcd_strings("ABCABD", "ABC")).toEqual("")
+// GCD: 3
+// i: 0, longer[i]: A, j: 0, shorter[j]: A
+// i: 1, longer[i]: B, j: 1, shorter[j]: B
+// i: 2, longer[i]: C, j: 2, shorter[j]: C
+// i: 3, longer[i]: A, j: 0, shorter[j]: A
+// i: 4, longer[i]: B, j: 1, shorter[j]: B
+// i: 5, longer[i]: D, j: 2, shorter[j]: C
+
+// expect(gcd_strings("ABABAB", "ABAB")).toEqual("AB")
+// GCD: 2
+// i: 0, longer[i]: A, j: 0, shorter[j]: A
+// i: 1, longer[i]: B, j: 1, shorter[j]: B
+// i: 2, longer[i]: A, j: 0, shorter[j]: A
+// i: 3, longer[i]: B, j: 1, shorter[j]: B
+// i: 4, longer[i]: A, j: 0, shorter[j]: A
+// i: 5, longer[i]: B, j: 1, shorter[j]: B
+
+// expect(gcd_strings("ABCABDABCABD", "ABCABD")).toEqual("ABCABD")
+// GCD: 6
+// i: 0, longer[i]: A, j: 0, shorter[j]: A
+// i: 1, longer[i]: B, j: 1, shorter[j]: B
+// i: 2, longer[i]: C, j: 2, shorter[j]: C
+// i: 3, longer[i]: A, j: 3, shorter[j]: A
+// i: 4, longer[i]: B, j: 4, shorter[j]: B
+// i: 5, longer[i]: D, j: 5, shorter[j]: D
+// i: 6, longer[i]: A, j: 0, shorter[j]: A
+// i: 7, longer[i]: B, j: 1, shorter[j]: B
+// i: 8, longer[i]: C, j: 2, shorter[j]: C
+// i: 9, longer[i]: A, j: 3, shorter[j]: A
+// i: 10, longer[i]: B, j: 4, shorter[j]: B
+// i: 11, longer[i]: D, j: 5, shorter[j]: D
