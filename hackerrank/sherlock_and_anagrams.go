7803f6ed010d06bc0a05968300b7310d6ef59db4
Raymond Gan <rayning@yahoo.com>
2020-03-14 22:40:40 -0700

    HR: Sherlock and Anagrams

---
+++ hackerrank/sherlock_and_anagrams.go
@@ -0,0 +1,55 @@
+// https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem
+func sherlockAndAnagrams(s string) int {
+    ssmap := make(map[[26]int]int) // substring frequency map
+
+    // loop through all possible substrings
+    for start := 0; start < len(s); start++ {
+        for end := start; end < len(s); end++ {
+            substr := s[start:end + 1]
+            ssmap[freqArray(substr)]++
+        }
+    }
+    delete(ssmap, freqArray(s)) // don't count original string s
+
+    numPairs := 0
+    for _, freq := range ssmap {
+		// Each substring's frequency has "n choose 2" pair combinations.
+		// combination("n choose 2") = n!/2!(n-2)! = n(n-1)/2
+        numPairs += freq * (freq - 1) / 2
+    }
+
+    return numPairs
+}
+
+func charToIndex(char rune) int {
+    return int(rune(char) - 97)
+}
+func freqArray(s string) [26]int {
+    var freq [26]int
+    for _, char := range s {
+        i := charToIndex(char)
+        freq[i]++
+    }
+    return freq
+}
+
+// func isAnagram(s, t string) bool {
+//     if len(s) != len(t) {
+//         return false
+//     }
+//     freq := make(map[rune]int)
+//     runes := []rune(s)
+//     runet := []rune(t)
+//     for i := 0; i < len(runes); i++ {
+//         schar := runes[i]
+//         tchar := runet[i]
+//         freq[schar]++
+//         freq[tchar]--
+//     }
+//     for _, val := range freq {
+//         if val != 0 {
+//             return false
+//         }
+//     }
+//     return true
+// }
