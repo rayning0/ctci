b5511d0e1ed437388a47c958ad4ebd1d0a9e9231
Raymond Gan <rgan@tubi.tv>
2022-04-11 04:18:26 -0700

    LC: Rotate Array: Add failed pop-shift answer

--- leetcode/medium/rotate_array.js
+++ leetcode/medium/rotate_array.js
@@ -1,13 +1,30 @@
 // https://leetcode.com/problems/rotate-array/
-// https://leetcode.com/submissions/detail/678233989/
+// https://leetcode.com/submissions/detail/678248500/
 // 3 companies asking it: Amazon, Microsoft, Facebook
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {void} Do not return anything, modify nums IN-PLACE instead!!!
  */
-// Solution 1:
-// Simplest answer.
+
+// 1. BEST! O(n) time, O(1) space
+let rotate = function(nums, k) {
+    nums.reverse()
+    k = k % nums.length
+    rev(nums, 0, k - 1)
+    rev(nums, k, nums.length - 1)
+}
+
+function rev(nums, start, end) {
+    while (start < end) {
+        [nums[start], nums[end]] = [nums[end], nums[start]]
+        start++
+        end--
+    }
+}
+
+
+// 2. Simpler
 // O(n) time, O(n) space
 let rotate = function(nums, k) {
     let temp = [...nums]
@@ -26,33 +43,11 @@ function whereItGoes(start, k, length) {
     }
 }
 
-// Solution 2:
-// O(n) time, O(n) space
+// Simplest, but this FAILS due to new LeetCode case that exceeds time limit
 let rotate = function(nums, k) {
-    const len = nums.length
-    let destination = whereItGoes(0, k, len)
-    let sliceindex = len - destination
-
-    const left = nums.slice(sliceindex)
-    nums.unshift(...left)
-    nums.splice(len)
-}
-
-// Solution 3. Do 3 reverses.
-// BEST ANSWER.
-// O(n) time, O(1) space
-let rotate = function(nums, k) {
-    nums.reverse()
     k = k % nums.length
-    rev(nums, 0, k - 1)
-    rev(nums, k, nums.length - 1)
-}
-
-// Reverse array between start + end indices
-function rev(nums, start, end) {
-    while (start < end) {
-        [nums[start], nums[end]] = [nums[end], nums[start]]
-        start++
-        end--
+    while (k > 0) {
+        nums.unshift(nums.pop())
+        k--
     }
 }
