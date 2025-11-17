57f6d871d0b89a60689a7460180f43cda0df7c3a
Raymond Gan <rgan@tubi.tv>
2025-11-17 00:37:31 -0800

    Binary Search

---
+++ neetcode/027_binary_search_spec.rb
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/binary-search/?envType=problem-list-v2&envId=plakya4j
+
+# Time: O(log n), Space: O(1)
+def search(nums, target)
+  left, right = 0, nums.size - 1
+
+  while left <= right # need "<=" to solve odd sized arrays
+    mid = (left + right) / 2
+
+    if nums[mid] < target
+      left += 1
+    elsif nums[mid] > target
+      right -= 1
+    else
+      return mid
+    end
+  end
+
+  -1
+end
+
+describe 'binary search' do
+  it '' do
+    expect(search([5], 5)).to eq 0
+    expect(search([-1,0,3,5,9], 9)).to eq 4 # tricky for odd size
+    expect(search([-1,0,3,5,9,12], 9)).to eq 4
+    expect(search([-1,0,3,5,9,12], 2)).to eq -1
+    expect(search([-1,0,2,4,6,8], 4)).to eq 3
+    expect(search([-1,0,2,4,6,8], 3)).to eq -1
+  end
+end
