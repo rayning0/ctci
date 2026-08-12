54352a631304cfcc14c8f3aae01983029aa7f86d
Raymond Gan <rayning@gmail.com>
2026-08-11 23:27:07 -0700

    Update 'Product of Array Except Self'

--- neetcode/238_product_of_array_except_self.py
+++ neetcode/238_product_of_array_except_self.py
@@ -1,12 +1,13 @@
 # https://leetcode.com/problems/product-of-array-except-self/?envType=problem-list-v2&envId=plakya4j
 # https://neetcode.io/solutions/product-of-array-except-self
 
+#           0      1          2           3        = i
 #          [4,     5,         10,         15  ]    = nums
 #     [     1      1*4        1*4*5     1*4*5*10 ] = left product (prefix)
 #     [ 1*5*10*15  1*10*15    1*15         1     ] = right product (suffix)
 # __________________________________________________
 # Multiply down vertically:
-# Res=[1*5*10*15  1*4*10*15   1*4*5*15  1*4*5*10 ]
+#      [1*5*10*15  1*4*10*15  1*4*5*15  1*4*5*10 ] = Answer
 
 # def productExceptSelf(nums: list[int]) -> list[int]:
 #     size = len(nums)
@@ -22,10 +23,10 @@
 #     return [left[i] * right[i] for i in range(size)]
 #     # return [l * r for l, r in zip(left, right)] <--- could use zip()
 
-# Time: O(n), Space: O(n)
+# Time: O(n), Space: O(n) for output prefix/suffix lists, O(1) for extra space
 def productExceptSelf(nums: list[int]) -> list[int]:
     size = len(nums)
-    left, right = 1, 1
+    left = right = 1
     prefix, suffix = [1] * size, [1] * size
 
     for i in range(1, size):
@@ -36,27 +37,15 @@ def productExceptSelf(nums: list[int]) -> list[int]:
         right *= nums[i + 1]
         suffix[i] = right
 
+    # print (f"prefix: {prefix}")
+    # print (f"suffix: {suffix}")
+
     return [prefix[i] * suffix[i] for i in range(size)]
 
 
 if __name__ == "__main__":
+    assert productExceptSelf([4, 5, 10, 15]) == [750, 600, 300, 200]
     assert productExceptSelf([5, 6, 2, 10]) == [120, 100, 300, 60]
     assert productExceptSelf([1, 2, 3, 4]) == [24, 12, 8, 6]
     assert productExceptSelf([-1, 1, 0, -3, 3]) == [0, 0, 9, 0, 0]
     print("All tests passed!")
-
-# You can't do this!
-# arr = []
-# arr[0] = 5
-# arr[1] = 6
-
-# Python lists don't auto-expand on index assignment. You must either create list with required size or use append() to add new elements!
-# arr = [0] * 2
-# arr[0] = 5
-# arr[1] = 6
-
-# OR
-
-# arr = []
-# arr.append(5)
-# arr.append(6)
