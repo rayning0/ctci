d92ca66faa8b71bac6184bce0de0365a3d834d6f
Raymond Gan <rayning@gmail.com>
2026-08-22 13:08:34 -0700

    Clarify + improve 'Search in Rotated Sorted Array'. Much easier to remember

--- neetcode/033_search_in_rotated_sorted_array.py
+++ neetcode/033_search_in_rotated_sorted_array.py
@@ -1,46 +1,51 @@
 # https://leetcode.com/problems/search-in-rotated-sorted-array/
-# https://neetcode.io/solutions/search-in-rotated-sorted-array (Binary Search)
+# https://neetcode.io/solutions/search-in-rotated-sorted-array
+# Binary Search: Exact Match
 
 # Ex: [4,5,6,7,0,1,2] # nums[mid] > nums[-1]. pivot = 4. It's really
 # [0,1,2,4,5,6,7] rotated left by pivot - 1 = 3
 
+# Just remember flowchart:
+# 1. Find mid
+# 2. Is num[mid] == target? If yes, return mid.
+# 3. Which side is sorted?
 
+# 4. LEFT (nums[l] <= nums[mid])
+# 5. Is target in LEFT side? (nums[l] <= target and target <= nums[mid])
+# 6. YES: go left
+# 7. NO: go right.
+
+# 4. RIGHT
+# 5. Is target in RIGHT side? (nums[mid] <= target and target <= nums[r])
+# 6. YES: go right
+# 7. NO: go left
+
+# Time: O(log n), Space: O(1)
 def search(nums: list[int], target: int) -> int:
     l, r = 0, len(nums) - 1
-
-    # Find index of pivot (smallest element). pivot is where nums suddenly drop.
     while l <= r:
         mid = (l + r) // 2
-        if nums[mid] > nums[-1]:
-            l = mid + 1  # pivot is to right
-        else:
-            r = mid - 1
-    pivot = l
+        if nums[mid] == target:
+            return mid
 
-    # inner function
-    def bsearch(l, r, target):
-        while l <= r:
-            mid = (l + r) // 2
-            if nums[mid] < target:
-                l = mid + 1
-            elif nums[mid] > target:
-                r = mid - 1
+        if nums[l] <= nums[mid]:    # LEFT side is sorted
+            if nums[l] <= target <= nums[mid]:   # target in LEFT side
+                r = mid - 1                      # Go LEFT
             else:
-                return mid
-        return -1
+                l = mid + 1                      # Go RIGHT
 
-    # Do binary search over 2 halves:
-    # 1. Left of pivot
-    index = bsearch(0, pivot - 1, target)
-    if index != -1:
-        return index
+        else:                       # RIGHT side is sorted
+            if nums[mid] <= target <= nums[r]:   # target in RIGHT side
+                l = mid + 1                      # Go RIGHT
+            else:
+                r = mid - 1                      # Go LEFT
 
-    # 2. From pivot to right
-    return bsearch(pivot, len(nums) - 1, target)
-
+    return -1
 
 if __name__ == "__main__":
     assert search([4, 5, 6, 7, 0, 1, 2], 0) == 4
     assert search([4, 5, 6, 7, 0, 1, 2], 3) == -1
     assert search([1], 0) == -1
+    assert search([3,4,5,6,1,2], 1) == 4
+    assert search([3,5,6,0,1,2], 4) == -1
     print("All tests passed!")
