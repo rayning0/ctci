3ccbe3a73baf4353a7569993581e9ed0c2701b66
Raymond Gan <rayning@gmail.com>
2026-08-22 00:06:25 -0700

    Clarify 'Binary Search': EXACT MATCH pattern

--- neetcode/704_binary_search.py
+++ neetcode/704_binary_search.py
@@ -1,19 +1,21 @@
 # https://leetcode.com/problems/binary-search/?envType=problem-list-v2&envId=plakya4j
 # https://neetcode.io/problems/binary-search/question?list=neetcode150
-# Binary Search. Data structure must be SORTED (or monotonic).
+# Binary Search: Exact Match
+# (Data structure must be SORTED (or monotonic).)
 
 # Time: O(log n), Space: O(1)
 def search(nums: list[int], target: int) -> int:
     l, r = 0, len(nums) - 1
 
-    while l <= r:   # use "l <= r" if we seek target in CLOSED interval [l, r]
+    while l <= r:               # use for EXACT MATCH
         mid = (l + r) // 2
-        if nums[mid] < target:
-            l = mid + 1     # do NOT use l +=1, which is O(n). mid eliminates half of search space, O(log n).
-        elif nums[mid] > target:
-            r = mid - 1     # do NOT use r -= 1
-        else:
+
+        if nums[mid] == target: # use for EXACT MATCH
             return mid
+        elif nums[mid] < target:
+            l = mid + 1     # do NOT use l +=1, which is O(n). mid eliminates half of search space, O(log n).
+        else:
+            r = mid - 1
 
     return -1
 
@@ -26,14 +28,27 @@ if __name__ == "__main__":
     assert search([-1, 0, 2, 4, 6, 8], 3) == -1
     print("All tests passed!")
 
-# Use "while l < r" if we seek a BOUNDARY or INSERTION index, not exact match.
-# Ex: In these cases , l naturally converges to answer without extra bookkeeping.
-# 1. lower bound: first element >= target
-# 2. Upper bound: first element > target
-# 3. Search insert position
-# https://leetcode.com/problems/search-insert-position/description/
+# For questions like:
+# "Find FIRST value that meets a condition."
+# "Find first place I can legally INSERT target."
+# "Keep any value that could still be the answer."
+# "What's the smallest capacity that works?"
 
-# Goal                                  Use
-# Is target in array? (Exact match)     [l, r] with l <= r
-# Where should target go (Boundary)     [l, r) with l < r
-# With [l, r), when loop ends, l is answer. It points to boundary.
+# Use LOWER BOUND pattern instead!
+# Most interview binary search problems are LOWER BOUND, not EXACT MATCH.
+
+# "Lower Bound": first index when nums[i] >= target.
+# Instead of exact match, LOWER BOUND says:
+# “This could be the answer, but maybe there’s another valid answer to its left.”
+# So it: 1) Saves current answer. 2) Keeps searching on its left.
+
+# l, r = 0, len(nums)
+
+# while l < r:
+#     mid = (l + r) // 2
+#     if nums[mid] < target:
+#         l = mid + 1
+#     else:                   <---- nums[mid] >= target
+#         r = mid
+
+# return l                    <--- when l == r
