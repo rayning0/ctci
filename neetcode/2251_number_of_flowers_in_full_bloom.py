e75c87f4433a7dce96371d4d244092828bf2e5e2
Raymond Gan <rayning@gmail.com>
2026-09-04 13:11:08 -0700

    Give bisect_right(), bisect_left() solution to 'Number of Flowers in Full Bloom', with clear, detailed notes

--- neetcode/2251_number_of_flowers_in_full_bloom.py
+++ neetcode/2251_number_of_flowers_in_full_bloom.py
@@ -1,20 +1,23 @@
 # https://leetcode.com/problems/number-of-flowers-in-full-bloom/description/
 # https://neetcode.io/solutions/number-of-flowers-in-full-bloom
-# Intervals: Active Intervals + Binary Search
+# Intervals: Upper + Lower Bound Binary Search
 
-# INSIGHT: At given time, blooming flowers a person sees =
+# INSIGHT: Blooming flowers a person sees at time =
 # flowers that started blooming - flowers that stopped blooming.
 # Binary search list of sorted start times to count flowers that started blooming at time.
 # Binary search list of sorted end times to count flowers that stopped blooming by time.
 
 # For flower to bloom, start <= time <= end
-# flowers started blooming = count(start <= time)
-# flowers stopped blooming = count(end < time)
+# flowers started blooming = count(start <= time) = first value > time
+# flowers stopped blooming = count(end < time) = first value >= time
 
 # lower_bound(time) = first value >= time
 #                   = count of values < time
+#                   = bisect_left(end, time)
+
 # upper_bound(time) = first value > time
 #                   = count of values <= time
+#                   = bisect_right(start, time)
 
 # Remember, any index in list = # of items that come before it.
 
@@ -25,15 +28,20 @@
 # If person looks at time 7,
 # How many flowers already blooming?            start <= 7.
 # [1, 3, 4 | 9] = 3 flowers
-# count of values <= 7 is upper_bound(7) = first value > 7.
+# count of values <= 7 | upper_bound(7) = first value > 7 | bisect_right(start, 7) all mean SAME!
+
 
 # How many flowers already stopped blooming?    end < 7.
 # [6 | 7, 12, 13] = 1 flower
-# count of values < 7 is lower_bound(7) = first value >= 7.
+# count of values < 7 | lower_bound(7) = first value >= 7 | bisect_left(end, 7) all mean SAME!
 
 # So actual flowers blooming at time 7 = 3 - 1 = 2 flowers.
 
 # 1. Binary Search (2 sorted arrays)
+from bisect import bisect_left, bisect_right
+
+# Time: O((n + m) log n), Space: O(n) auxiliary
+# n = len(flowers), m = len(people)
 def fullBloomFlowers(flowers: list[list[int]], people: list[int]) -> list[int]:
     start, end, ans = [], [], []
     for s, e in flowers:
@@ -43,31 +51,34 @@ def fullBloomFlowers(flowers: list[list[int]], people: list[int]) -> list[int]:
     end.sort()
 
     for time in people:
-        # Binary Search: count of start values <= time is same as
+        # flowers start blooming when start times <= time
         # upper_bound(time) = first value > time
-        l, r = 0, len(start)
-        while l < r:
-            mid = (l + r) // 2
-            if start[mid] > time:
-                r = mid
-            else:
-                l = mid + 1
+        started = bisect_right(start, time)
 
-        started = l   # flowers that started blooming by time
-        print(f"started blooming: {started}, time: {time}")
+        # l, r = 0, len(start)
+        # while l < r:
+        #     mid = (l + r) // 2
+        #     if start[mid] > time:
+        #         r = mid
+        #     else:
+        #         l = mid + 1
 
-        # Binary Search: count of end values < time is same as
+        # started = l
+
+        # flowers stop blooming when end times < time
         # lower_bound(time) = first value >= time
-        l, r = 0, len(end)
-        while l < r:
-            mid = (l + r) // 2
-            if end[mid] >= time:
-                r = mid
-            else:
-                l = mid + 1
+        ended = bisect_left(end, time)
 
-        ended = l     # flowers that stopped blooming by time
-        print(f"ended blooming: {ended}, time: {time}")
+        # l, r = 0, len(end)
+        # while l < r:
+        #     mid = (l + r) // 2
+        #     if end[mid] >= time:
+        #         r = mid
+        #     else:
+        #         l = mid + 1
+
+        # ended = l
+
         ans.append(started - ended)
 
     return ans
