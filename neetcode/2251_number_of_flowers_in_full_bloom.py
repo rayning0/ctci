3bfbeed2297a1c1bac897142f22ff08de2096a10
Raymond Gan <rayning@gmail.com>
2026-09-04 15:17:58 -0700

    Add 'Sweep Line (Event List)' solution to 'Number of Flowers in Full Bloom'

--- neetcode/2251_number_of_flowers_in_full_bloom.py
+++ neetcode/2251_number_of_flowers_in_full_bloom.py
@@ -37,7 +37,7 @@
 
 # So actual flowers blooming at time 7 = 3 - 1 = 2 flowers.
 
-# 1. Binary Search (2 sorted arrays)
+# 1. Binary Search (2 sorted arrays). BEST for interview!
 from bisect import bisect_left, bisect_right
 
 # Time: O((n + m) log n), Space: O(n) auxiliary
@@ -83,8 +83,69 @@ def fullBloomFlowers(flowers: list[list[int]], people: list[int]) -> list[int]:
 
     return ans
 
-# 2. Sweep Line (event list)
-# def fullBloomFlowers(flowers: list[list[int]], people: list[int]) -> list[int]:
+# 2. Sweep Line (event list). Hard to remember.
+# See "Sweep Line with sorted event list" answer:
+# https://github.com/rayning0/ctci/blob/e75c87f4433a7dce96371d4d244092828bf2e5e2/neetcode/1094_car_pooling.py#L33
+
+# Sort flower events and people by time. Sweep through both once, processing every flower event before answering each person's query.
+
+3 event types:
+1. flower starts bloom:  +1 blooming flower   = (start time,   +1)
+2. flower stops bloom:   -1 blooming flower   = (end time + 1, -1). Use "end time + 1" since flowers still bloom at end time.
+3. person arrives to ask current bloom count  = (time, person index)
+
+Example:
+flowers = [[1,6], [3,7], [4,5]]
+people = [2,5,7]
+
+Events:
+start = [(1, +1), (3, +1), (4, +1)]
+end   = [(7, -1), (8, -1), (6, -1)]
+asks  = [(2, 0), (5, 1), (7, 2)]
+
+Sort all events by time:
+
+Time    Event           Change      Blooming Flowers
+1       flower starts   +1          1
+2       person asks                 1   *
+3       flower starts   +1          2
+4       flower starts   +1          3
+5       person asks                 3   *
+6       flower ends     -1          2
+7       flower ends     -1          1
+7       person asks                 1   *
+8       flower ends     -1          0
+
+ans = [1, 3, 1]
+
+# Time: O(n log n + m log m), Space: O(n + m) auxiliary
+# n = len(flowers), m = len(people)
+def fullBloomFlowers(flowers: list[list[int]], people: list[int]) -> list[int]:
+    flower_events, asks = [], []
+    ans = [0] * len(people)
+
+    for start, end in flowers:
+        flower_events.append((start, 1))       # for each start time, 1 new flower blooms
+        flower_events.append((end + 1, -1))    # for each end + 1 time, 1 old flower stops blooming
+    flower_events.sort()
+
+    for i, time in enumerate(people):
+        asks.append((time, i))
+
+    asks.sort()
+
+    blooming = j = 0
+    for time, person_index in asks:
+
+        # Loop through flower events for all times <= each person's ask time
+        while j < len(flower_events) and flower_events[j][0] <= time:
+            # add flower event's change value (+1 or -1)
+            blooming += flower_events[j][1]
+            j += 1
+
+        ans[person_index] = blooming
+
+    return ans
 
 # 3. Brute Force
 # Time: O(n * m). n = len(flowers), m = len(people)
